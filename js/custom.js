b930460a5a5295f8abc963599b68220ccfcb64b8
pinky-nadar <umamaheshwari4795@gmail.com>
2024-11-30 19:23:19 -0500

    sat eve

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,10 @@
-$('.tabs li a').click(function(){
+$('.tabs li a').click(function(event){
     $(this).parent().siblings().find("a").removeClass('active');
     $(this).addClass('active');
 
     $(this).parents('.work-container').find('.work').removeClass('active');
     var a = $(this).parent().index();
+    
     $('.work').each(function(){
         console.log($(this).index());
         if($(this).index() - 1 == a){
